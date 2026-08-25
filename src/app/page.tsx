5c29576ad077e12a606b692914a09531f85b0303
AyushMehtoliya <amehtoliya@gmail.com>
2026-08-25 15:44:44 +0530

    fix: remove leftover git merge conflict markers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,16 +5,9 @@ import { Navbar } from "@/components/navbar";
 
 export default function Home() {
   return (
-<<<<<<< HEAD
     <div className="relative flex flex-col flex-1 items-center justify-center bg-zinc-50 font-audiowide dark:bg-black min-h-screen">
       <Navbar />
-
       <Hero />
-=======
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-audiowide dark:bg-black">
-      <Navbar/>
-      <Hero/>
->>>>>>> 2a71d32d2aa809a7bae550efee5f1d6aebb82f80
     </div>
   );
 }
